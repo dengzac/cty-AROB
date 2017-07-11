52291ca9e77587b193d1f82858cba16e66a18f4e
Zachary Deng <dengzac@gmail.com>
2017-07-11 16:17:41 -0400

    Add inverse kinematics

--- libs/forwardKinematics.py
+++ libs/forwardKinematics.py
@@ -1,28 +1,52 @@
 import math
 import numpy as np
-from PiStorms import PiStorms
+#from PiStorms import PiStorms
 from PID import PIDController
 import time
-psm = PiStorms()
+#psm = PiStorms()
 #psm.BAM1.setSpeed(-100)
 #psm.BAM2.setSpeed(-100)
 #psm.BBM1.setSpeed(-100)
-startpos = [psm.BAM1.pos(), psm.BAM2.pos(), psm.BBM1.pos()]
-controllers = [PIDController(Scalar=1), PIDController(Scalar=1), PIDController(Scalar=1)]
-try:
-    while True:
-        curpos = [psm.BAM1.pos() - startpos[0], psm.BAM2.pos() - startpos[1], psm.BBM1.pos() - startpos[2]]
-        output = [controllers[0].step(0  - curpos[0]),controllers[1].step(0  - curpos[1]),controllers[2].step(0  - curpos[2])]
+class Arm(object):
+    def __init__(self):
+        self.startpos = [psm.BAM1.pos(), psm.BAM2.pos(), psm.BBM1.pos()]
+        self.controllers = [PIDController(Scalar=1), PIDController(Scalar=1), PIDController(Scalar=1)]
+
+        self.setpoints = [0,0,0]
+
+    def setSetPoints(self, angles):
+        self.setpoints = list(angles)
+    def step(self):
+        curpos = [psm.BAM1.pos() - self.startpos[0], psm.BAM2.pos() - self.startpos[1], psm.BBM1.pos() - self.startpos[2]]
+        output = [self.controllers[0].step(self.setpoints[0]  - curpos[0]),self.controllers[1].step(self.setpoints[1]  - curpos[1]),self.controllers[2].step(self.setpoints[2]  - curpos[2])]
         for i in range(3):
             if abs(output[i]) > 100:
                 output[i] = 100 * np.sign(output[i])
         print output
         #print "pos",psm.BAM1.pos(), psm.BAM2.pos(), psm.BBM1.pos()
-        psm.BAM1.setSpeed(output[0])
-        psm.BAM2.setSpeed(output[1])
-        psm.BBM1.setSpeed(output[2])
-        
-        time.sleep(0.05)
+        if abs(output[0]) > 10:
+            psm.BAM1.setSpeed(output[0])
+        else:
+            psm.BAM1.brake()
+
+        if abs(output[1]) > 10:
+            psm.BAM2.setSpeed(output[1])
+        else:
+            psm.BAM2.brake()
+
+        if abs(output[2]) > 10:
+            psm.BBM1.setSpeed(output[2])
+        else:
+            psm.BBM1.brake()
+try:
+    arm = Arm()    
+except:
+    pass
+try:
+    pass
+    # while True:
+    #     arm.step()
+    #     time.sleep(0.5)
 except:
     psm.BAM1.setSpeed(0)
     psm.BAM2.setSpeed(0)
@@ -56,6 +80,17 @@ def forwardKinematicsMatrix(segments,point):
     rotated = np.dot(rotation, newpoint)
     print "new point", rotated
     forwardKinematicsMatrix(segments[:-1], rotated)
+
+def lawCosines(dist, l1, l2): # Gets angle opposite to segment of length dist
+    return math.degrees(math.acos(float(l1**2 + l2**2 - dist**2) / float(2*l1*l2)))
+def inverseKinematics(l1, l2, x, y):
+    thetaEnd = math.degrees(math.atan2(y, x))
+    distEnd = math.sqrt(x**2 + y **2)
+    theta1 = lawCosines(l2, distEnd, l1)
+    theta2 = lawCosines(distEnd, l2, l1)
+    return (theta1 + thetaEnd,-theta2)
+
+print inverseKinematics(10, 10, 10, 10)
 print forwardKinematicsMatrix([[45, 2],[0, 1], [10, 7]], np.array([[0],[0]]))#degrees, length
 
 print forwardKinematics([[45, 2],[0, 1], [10, 7]])
